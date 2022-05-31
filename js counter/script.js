cb3f6f26db103b7761c81270747de129b8824470
yaser <ddemety372@gmail.com>
2022-05-31 07:41:08 +0300

    Js finished

--- js counter/script.js
+++ js counter/script.js
@@ -1,1 +1,33 @@
 
+const increase = document.querySelector(".inc");
+const decrease = document.querySelector(".dec");
+const resetle = document.querySelector(".reset");
+let firstValue = 0;
+const shower = document.getElementById("num");
+
+increase.addEventListener("click", () => {
+  firstValue++;
+  shower.textContent = firstValue;
+  if (firstValue > 0 && firstValue < 10) {
+    shower.style.color = "green";
+  } else if (firstValue > 10) {
+    shower.style.color = "purple";
+  } else if (firstValue < 0) {
+    shower.style.color = "red";
+  }
+});
+
+decrease.addEventListener("click", () => {
+  firstValue--;
+  shower.textContent = firstValue;
+  if (firstValue > 0) {
+    shower.style.color = "green";
+  } else if (firstValue < 0) {
+    shower.style.color = "red";
+  }
+});
+
+resetle.addEventListener("click", () => {
+  firstValue = 0;
+  shower.textContent = firstValue;
+});
